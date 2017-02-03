a7e3c95695abde344b239af662678d2742f169b4
Lucas Ramos <lucasrcdias@gmail.com>
2017-02-03 14:01:10 -0200

    Fix tabs

--- src/js/closestPolyfill.js
+++ src/js/closestPolyfill.js
@@ -1,6 +1,6 @@
 (function () {
 	if (window.Element && !Element.prototype.closest) {
-    Element.prototype.closest = 
+    Element.prototype.closest =
     function(s) {
       var matches = (this.document || this.ownerDocument).querySelectorAll(s);
       var i;
@@ -9,8 +9,8 @@
       do {
         i = matches.length;
         while (--i >= 0 && matches.item(i) !== el) {};
-      } while ((i < 0) && (el = el.parentElement)); 
-      
+      } while ((i < 0) && (el = el.parentElement));
+
       return el;
     }
 	}
